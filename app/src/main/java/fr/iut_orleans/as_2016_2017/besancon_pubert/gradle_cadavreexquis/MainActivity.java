4bcf57b2fab1384bde684dda6f8cadfeddd3ebf3
Mathieu Pubert <mathieu.pubert@etu.univ-orleans.fr>
2017-05-27 22:29:38 +0200

    Preparation de MainActivity a aller chercher le nom de l'utilisateur

--- app/src/main/java/fr/iut_orleans/as_2016_2017/besancon_pubert/gradle_cadavreexquis/MainActivity.java
+++ app/src/main/java/fr/iut_orleans/as_2016_2017/besancon_pubert/gradle_cadavreexquis/MainActivity.java
@@ -1,13 +1,41 @@
 package fr.iut_orleans.as_2016_2017.besancon_pubert.gradle_cadavreexquis;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import fr.iut_orleans.as_2016_2017.besancon_pubert.gradle_cadavreexquis.models.Utilisateur;
+
 public class MainActivity extends AppCompatActivity {
+
+    Utilisateur utilisateur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Si un utilisateur est connecté :
+        if(savedInstanceState.get("Utilisateur")!=null)
+        {
+
+        }
+        // Sinon on envoie Anonyme se connecter
+        else{
+            utilisateur = this.getUtilisateur();
+        }
+    }
+
+    public Utilisateur getUtilisateur(){
+
+        Utilisateur utilisateur = null;
+        Intent intent = new Intent("Connexion Utilisateur", Uri.parse("Utilisateur : " + utilisateur), this , LoginScreen.class);
+
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }
+
+        return utilisateur;
     }
 }
