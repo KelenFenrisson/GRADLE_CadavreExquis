90fdca5393d0895f9fb596c4d2be804d7419d60e
Besancon <besancon.julien@gmail.com>
2017-05-28 21:37:11 +0200

    test ajout dans dbb

--- app/src/main/java/fr/iut_orleans/as_2016_2017/besancon_pubert/gradle_cadavreexquis/Inscription.java
+++ app/src/main/java/fr/iut_orleans/as_2016_2017/besancon_pubert/gradle_cadavreexquis/Inscription.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import fr.iut_orleans.as_2016_2017.besancon_pubert.gradle_cadavreexquis.models.CadavreExquisBDD;
+import fr.iut_orleans.as_2016_2017.besancon_pubert.gradle_cadavreexquis.models.Utilisateur;
+
 public class Inscription extends Activity
 {
     /** Called when the activity is first created. */
@@ -17,16 +20,27 @@ public class Inscription extends Activity
     }
 
     public void validerInscription(View view){
+        EditText login = (EditText)findViewById(R.id.editLogin);
         EditText password = (EditText)findViewById(R.id.editPassword);
         EditText passwordConfirm = (EditText)findViewById(R.id.editPasswordCondfirm);
+        String textLogin = login.getText().toString();
+        String textPassword = password.getText().toString();
+
+        CadavreExquisBDD cadavreExquisBDD = new CadavreExquisBDD(this);
+        cadavreExquisBDD.open();
+
         if(!password.getText().toString().equals(passwordConfirm.getText().toString()))
         {
             Toast toast = Toast.makeText(this, "Pas le mot de passe identique", Toast.LENGTH_LONG);
             toast.show();
         }
+        else if (cadavreExquisBDD.getUtilisateurWithLogin(textLogin).equals(textLogin));
         else
         {
             //inscription a faire dans la base de données
+            Utilisateur utilisateur = new Utilisateur(textLogin,textPassword);
+            cadavreExquisBDD.insertUtilisateur(utilisateur);
+            cadavreExquisBDD.close();
             super.finish ();
         }
     }
