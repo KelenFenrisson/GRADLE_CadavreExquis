dd0269653a3a2ab00bf215a84c209c01ee7864ec
Besancon <besancon.julien@gmail.com>
2017-05-28 21:37:11 +0200

    test ajout dans dbb
    (cherry picked from commit 90fdca5)

--- app/src/main/java/fr/iut_orleans/as_2016_2017/besancon_pubert/gradle_cadavreexquis/Inscription.java
+++ app/src/main/java/fr/iut_orleans/as_2016_2017/besancon_pubert/gradle_cadavreexquis/Inscription.java
@@ -1,9 +1,13 @@
 package fr.iut_orleans.as_2016_2017.besancon_pubert.gradle_cadavreexquis;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import fr.iut_orleans.as_2016_2017.besancon_pubert.gradle_cadavreexquis.models.CadavreExquisBDD;
+import fr.iut_orleans.as_2016_2017.besancon_pubert.gradle_cadavreexquis.models.Utilisateur;
 
 public class Inscription extends Activity
 {
@@ -13,12 +17,33 @@ public class Inscription extends Activity
     {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_inscription);
-        Intent intent = getIntent ();
     }
 
     public void validerInscription(View view){
-        Intent intent = new Intent ();
-        setResult(Activity.RESULT_OK , intent );
-        super.finish ();
+        EditText login = (EditText)findViewById(R.id.editLogin);
+        EditText password = (EditText)findViewById(R.id.editPassword);
+        EditText passwordConfirm = (EditText)findViewById(R.id.editPasswordCondfirm);
+        String textLogin = login.getText().toString();
+        String textPassword = password.getText().toString();
+
+        CadavreExquisBDD cadavreExquisBDD = new CadavreExquisBDD(this);
+        cadavreExquisBDD.open();
+
+        if(!password.getText().toString().equals(passwordConfirm.getText().toString()))
+        {
+            Toast toast = Toast.makeText(this, "le mot de passe est mal confirmé", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        else if (cadavreExquisBDD.inBDDUtilisateurWithLogin(textLogin)){
+            Toast toast = Toast.makeText(this, "Ce nom existe déjà", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        else
+        {
+            Utilisateur utilisateur = new Utilisateur(textLogin,textPassword);
+            cadavreExquisBDD.insertUtilisateur(utilisateur);
+            cadavreExquisBDD.close();
+            super.finish ();
+        }
     }
 }
